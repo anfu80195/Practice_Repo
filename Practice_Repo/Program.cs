a23ca41bf36ce6a67d7806cfaff7d8fdfdbf5139
Anika <anika.fuglsang@gmail.com>
2026-03-04 11:45:20 +0100

    Add project files.

---
+++ Practice_Repo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_Repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
