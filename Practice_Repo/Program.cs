22776953649be23f707959fdc196b63ed0a71427
Anika <anika.fuglsang@gmail.com>
2026-03-04 12:10:07 +0100

    Add Magic Coffee Program

--- Practice_Repo/Program.cs
+++ Practice_Repo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Practice_Repo
+﻿using System;
+
+namespace FunProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Magic Coffee Machine!");
+
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "!");
+
+            Random random = new Random();
+            int magicNumber = random.Next(1, 6);
+
+            Console.WriteLine("Rolling the magic number...");
+
+            if (magicNumber == 5)
+            {
+                Console.WriteLine("YOU WIN A FREE COFFEE!");
+            }
+            else
+            {
+                Console.WriteLine("No coffee today 😭");
+            }
+
+            Console.WriteLine("Magic number was: " + magicNumber);
         }
     }
 }
